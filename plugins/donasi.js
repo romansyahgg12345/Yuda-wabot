c8f1bc0aabbab4cce2cee432020c6cd1caad263f
Yuda Official <67179682+romansyahgg12345@users.noreply.github.com>
2022-02-26 22:16:16 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,16 +1,16 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `››╭─〘 *Donasi* 〙
 ╭╡
-│┝‷✧ *Pulsa:* 6285712420674
-│┝‷✧ *Dana:* 6285712420674
-│┝‷✧ *Linkaja:* 6285712420674
-│┝‷✧ *Gopay:* 6285712420674
-│┝‷✧ *Ovo:* 6285712420674
+│┝‷✧ *Pulsa Indosat:* 6285788612710
+│┝‷✧ *im3tri:* 62895416210179
+│┝‷✧ *Linkaja:* -
+│┝‷✧ *Gopay:* -
+│┝‷✧ *Ovo:* -
 │╰───···─────
 │⁺◛˖ Ingin Donasi? Chat
-│wa.me/6285712420674?text=kak+mau+donasi
+│wa.me/62895416210179?text=kak+mau+donasi
 ╰──────────···───╮
-▌│█║▌║▌║║▌║▌║█│▌
+▌
 `.trim(), `${wm}`, 'Owner', '.owner')
 
 module.exports = handler
